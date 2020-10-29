d84bdc2e16d08de381859048f0a72b682dea56c5
PolinaRrr <poniapocalypsisa@gmail.com>
2020-10-29 16:44:09 +0300

    com#2

--- app/src/main/java/com/example/android_part1_lesson_1/MainActivity.java
+++ app/src/main/java/com/example/android_part1_lesson_1/MainActivity.java
@@ -15,15 +15,76 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onButtonClick(View v) {
-        EditText el1 = (EditText) findViewById(R.id.num1);
-        EditText el2 = (EditText) findViewById(R.id.num2);
+    public TextView getResult() {
         TextView result = (TextView) findViewById(R.id.result);
-
-        int num1 = Integer.parseInt(el1.getText().toString());
-        int num2 = Integer.parseInt(el2.getText().toString());
-
-        int sum = num1 + num2;
-        result.setText(Integer.toString(sum));
+        return result;
     }
+
+
+    public int getFirstOperand() throws Exception {
+        EditText el1 = (EditText) findViewById(R.id.num1);
+        String num1 = el1.getText().toString();
+
+        if (!num1.equals("")) {
+            return Integer.parseInt(num1);
+        } else {
+            throw new Exception("Введите оба числа");
+        }
+    }
+
+    public int getSecondOperand() throws Exception {
+        EditText el2 = (EditText) findViewById(R.id.num2);
+        String num2 = el2.getText().toString();
+        if (!num2.equals("")) {
+            return Integer.parseInt(num2);
+        } else {
+            throw new Exception("Введите оба числа");
+        }
+
+    }
+
+    public void onButtonAddClick(View v) {
+        try {
+            int add = getFirstOperand() + getSecondOperand();
+            getResult().setText(Integer.toString(add));
+        } catch (Exception e) {
+            getResult().setText(e.getMessage());
+        }
+    }
+
+    public void onButtonSubClick(View v) {
+        try {
+            int sub = getFirstOperand() - getSecondOperand();
+            getResult().setText(Integer.toString(sub));
+        } catch (Exception e) {
+            getResult().setText(e.getMessage());
+        }
+
+    }
+
+    public void onButtonMultiClick(View v) {
+        try {
+            int multi = getFirstOperand() * getSecondOperand();
+            getResult().setText(Integer.toString(multi));
+        } catch (Exception e) {
+            getResult().setText(e.getMessage());
+        }
+
+    }
+
+    public void onButtonDivClick(View v) {
+        try {
+            if (getSecondOperand() != 0) {
+                int div = getFirstOperand() / getSecondOperand();
+                getResult().setText(Integer.toString(div));
+            } else {
+                getResult().setText("На ноль делить нельзя");
+            }
+
+        } catch (Exception e) {
+            getResult().setText(e.getMessage());
+        }
+
+    }
+
 }
