1104a838670f6402f6e040410014729050102ece
PolinaRrr <poniapocalypsisa@gmail.com>
2020-10-29 14:57:22 +0300

    com#1

--- app/src/main/java/com/example/android_part1_lesson_1/MainActivity.java
+++ app/src/main/java/com/example/android_part1_lesson_1/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.android_part1_lesson_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View v) {
+        EditText el1 = (EditText) findViewById(R.id.num1);
+        EditText el2 = (EditText) findViewById(R.id.num2);
+        TextView result = (TextView) findViewById(R.id.result);
+
+        int num1 = Integer.parseInt(el1.getText().toString());
+        int num2 = Integer.parseInt(el2.getText().toString());
+
+        int sum = num1 + num2;
+        result.setText(Integer.toString(sum));
     }
 }
